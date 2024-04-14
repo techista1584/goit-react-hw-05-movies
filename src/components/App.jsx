c12e1ab37d74edcf89e682100ca8449be25c70c0
Ruby Mateo <fangprincess_2k3@yahoo.com>
2024-04-14 21:26:14 +0800

    separate details page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/Home/HomePage';
 import { MoviesPage } from './pages/Movies/MoviesPage';
 import { MovieDetailsPage } from './pages/Movie/MovieDetailsPage';
-import { Header } from './Header/Header';
+import { SharedLayout } from './pages/Shared/SharedLayout';
 
 export const App = () => {
   return (
     <div>
-      <Header/>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />}>
-          <Route path=":movieId" element={<MovieDetailsPage />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />}/>
+          <Route path="/movies" element={<MoviesPage />}/>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
         </Route>
       </Routes>
     </div>

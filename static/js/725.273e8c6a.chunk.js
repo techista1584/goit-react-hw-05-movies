8f593fabb83658c9a7a905a5f6d61e58d292acf1
techista1584 <119954182+techista1584@users.noreply.github.com>
2024-04-17 18:54:40 +0000

    Deploying to gh-pages from @ techista1584/goit-react-hw-05-movies@c130e41a10b0f72e366f59237e6bda47f0522fc6 🚀

--- static/js/725.273e8c6a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[725],{725:function(e,t,i){i.r(t);i(791);var n=i(87),s=i(184);t.default=function(){return(0,s.jsxs)("div",{children:[(0,s.jsx)(n.rU,{to:"/movies",children:(0,s.jsx)("button",{children:"\u2b05 Go back"})}),(0,s.jsx)("p",{children:"NotFoundPage"})]})}}}]);
-//# sourceMappingURL=725.273e8c6a.chunk.js.map

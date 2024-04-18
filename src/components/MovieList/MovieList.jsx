75f5e56627e9c31ff1a371c227b3be88f2ffb504
Ruby Mateo <119954182+techista1584@users.noreply.github.com>
2024-04-18 18:24:02 +0800

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -24,7 +24,7 @@ export const MovieList = ({ movies }) => {
           {movies.map(({ id, title, poster_path }) => {
             return (
               <li className={css.li} key={id}>
-                <Link className={css.links} to={`/movies/${id}`} state={{ from: location }}>
+                <Link className={css.links} to={`/goit-react-hw-05-movies/movies/${id}`} state={{ from: location }}>
                   <div className={css.card}>
                     {poster_path ? (
                       <img

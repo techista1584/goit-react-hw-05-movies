e938428bbe9710f59e29020799e879cc48745ccc
Ruby Mateo <119954182+techista1584@users.noreply.github.com>
2024-04-18 15:06:12 +0800

    Update MovieListItem.jsx

--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -7,7 +7,7 @@ export const MovieListItem = ({ id, title }) => {
     // console.log("from location of movie list item",location)
     return (
         <li className={css.movieItem} key={id}>
-            <Link to={`/movies/${id}`} state={{from:location}}  className={css.movieLink}>
+            <Link to={`/goit-react-hw-05-movies/movies/${id}`} state={{from:location}}  className={css.movieLink}>
                 <h3>{title}</h3>
             </Link>
         </li>
